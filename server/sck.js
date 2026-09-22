0409ffab7d3adb973ee285ea0bea1635baa85ab0
Rishi <rishivarma.d@gmail.com>
2026-09-22 14:38:59 +0530

    doploy 1.0

--- server/sck.js
+++ server/sck.js
@@ -5,7 +5,9 @@ const initSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173",
+         "https://smart-booking-system-for-classes-and-labs-l2imw1r7n-trying13.vercel.app"
+      ],
       credentials: true
     }
   });
